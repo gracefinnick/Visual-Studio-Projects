1e54ac4db508d9c210a16e4eb4b1914aad375777
Grace Finnick <35904299+gracefinnick@users.noreply.github.com>
2018-07-18 10:49:26 -0400

    Practice with delegates and events

---
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public delegate int BizRulesDelegate(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var custs = new List<Customer>
+            {
+                new Customer { City = "Phoenix", FirstName = "John", LastName = "Doe", ID = 1},
+                new Customer { City = "Phoenix", FirstName = "Jane", LastName = "Doe", ID = 500},
+                new Customer { City = "Seattle", FirstName = "Suki", LastName = "Pizzoro", ID = 3},
+                new Customer { City = "New York City", FirstName = "Michelle", LastName = "Smith", ID = 4}
+            };
+            // Wrap down statements to increase readability
+            var phxCusts = custs
+                .Where(c => c.City == "Phoenix" && c.ID < 500)
+                .OrderBy(c => c.FirstName);
+            foreach ( var cust in phxCusts)
+            {
+                Console.WriteLine(cust.FirstName);
+            }
+
+            var data = new ProcessData();
+            // Create delegates that define the action we want to be taken
+            BizRulesDelegate addDel = (x, y) => x + y;
+            BizRulesDelegate multiplyDel = (x, y) => x * y;
+            data.Process(2, 3, addDel);
+
+            // Using Func, no defining the delegate
+            Func<int, int, int> funcAddDel = (x, y) => x + y;
+            Func<int, int, int> funcMultiplyDel = (x, y) => x * y;
+            data.ProcessFunc(3, 2, funcAddDel);
+
+            var worker = new Worker();
+            // Using lambda expressions
+            //Using brackets allows for multiple lines of code
+            worker.WorkPerformed += (s, e) =>
+            {
+                Console.WriteLine("Worked: " + e.Hours + " hour(s) doing: " + e.WorkType);
+                Console.WriteLine("Other value here");
+            };
+            worker.WorkCompleted += (s, e) => Console.WriteLine("Worker is done");
+            worker.DoWork(8, WorkType.GenerateReports);
+
+            // Keeps the window open
+            Console.Read();
+        }
+        
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
+
+
---
+++ DelegatesAndEvents/DelegatesAndEvents/Worker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    // Removed because working with directly in event WorkPerformed
+    // public delegate void WorkPerformedHandler(object sender, WorkPerformedEventArgs e);
+    public class Worker
+    {
+        // Directly working with event, avoid using delegate
+        // Good practice when delegate is not needed on its own somewhere
+        // Allows for delegate inference in Program class
+        public event EventHandler<WorkPerformedEventArgs> WorkPerformed;
+        public event EventHandler WorkCompleted;
+        public void DoWork(int hours, WorkType workType)
+        {
+            // Loop through and rais an event every hour
+            for (int i = 0; i < hours; i++)
+            {
+                // Makes system wait one second between each iteration
+                System.Threading.Thread.Sleep(1000);
+                OnWorkPerformed(i + 1, workType);
+            }
+            // Raise final completed event
+            OnWorkCompleted();
+        }
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            //Option 1: directly on the event
+            //if(WorkPerformed != null)
+            //{
+            //    WorkPerformed(hours, workType);
+            //}
+
+            //Option 2: cast as delegate
+            var del = WorkPerformed as EventHandler<WorkPerformedEventArgs>;
+            if (del != null)
+            {
+                del(this, new WorkPerformedEventArgs(hours, workType));
+            }
+        }
+        protected virtual void OnWorkCompleted()
+        {
+            var del = WorkCompleted as EventHandler;
+            if (del != null)
+            {
+                del(this, EventArgs.Empty);
+            }
+        }
+    }
+}
