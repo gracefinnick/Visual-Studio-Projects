cb9b41f7e1aba9f25c3b16c4ea6323e6a1c7a6d7
Grace Finnick <35904299+gracefinnick@users.noreply.github.com>
2018-07-18 10:52:12 -0400

    GradeBook application
    allows user to enter grades for a given student, compute statistics for those grades, also raises an event if the name of a grade book entry is changed

---
+++ GradeBook/GradeBook/GradeTracker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    public abstract class GradeTracker : IGradeTracker
+    {
+        //event
+        public event NameChangedDelegate NameChanged;
+        //To name the list
+        protected string _name;
+        public abstract void AddGrade(float grade);
+        public abstract GradeStatistics ComputeStatistics();
+        public abstract void WriteGrades(TextWriter destination);
+        //required due to inheriting enumerator in igradetracker
+        public abstract IEnumerator GetEnumerator();
+        //allows access to the private _name field
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty");
+                }
+
+                if (_name != value)
+                {
+                    NameChangedEventArgs args = new NameChangedEventArgs();
+                    args.ExistingName = _name;
+                    args.NewName = value;
+                    //this refers to the grades object that we're currently operating on
+                    //NameChanged(this, args);
+                }
+                _name = value;
+
+            }
+        }
+    }
+}
---
+++ GradeBook/GradeBook/ThrowAwayGradeBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    class ThrowAwayGradeBook : Grades
+    {
+        public override GradeStatistics ComputeStatistics()
+        {
+            float lowest = float.MaxValue;
+            foreach(float grade in grades)
+            {
+                lowest = Math.Min(grade, lowest);
+            }
+            grades.Remove(lowest);
+            return base.ComputeStatistics();
+        }
+    }
+}
